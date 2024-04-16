1c788564ff761a78a8c748b99605e48182c00c9d
Kalpit <kalpitchandekar1736@gmail.com>
2024-04-16 12:38:46 +0530

    refresh state hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
-
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  };
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+  const resetValue = () => {
+    setCounter(0);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload ...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter Value {counter}</h1>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
+      <button onClick={resetValue}>Reset value</button>
     </div>
   );
 }
